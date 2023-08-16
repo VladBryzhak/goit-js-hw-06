17743fc92aa3d88bff002e1c8f5d8fde8e62b004
VladBryzhak <124492057+VladBryzhak@users.noreply.github.com>
2023-08-16 17:30:03 +0300

    task-2

---
+++ js/task-02.js
@@ -0,0 +1,24 @@
+const ingredients = [
+  'Potatoes',
+  'Mushrooms',
+  'Garlic',
+  'Tomatos',
+  'Herbs',
+  'Condiments',
+];
+
+const list = document.querySelector('#ingredients')
+
+function createMarkup(arr) {
+const markup = arr.map(ingredient => {
+  const li = document.createElement('li')
+  li.textContent = ingredient;
+  li.classList.add('item');
+
+  return li;
+});
+
+list.append(...markup)
+};
+
+createMarkup(ingredients)
